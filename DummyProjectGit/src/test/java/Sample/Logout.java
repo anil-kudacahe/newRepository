3fa712398bf8c5f13f534296fcab46f5192158fc
DELL <DELL@DESKTOP-A7DG58Q>
2022-08-29 12:37:12 +0530

    sdfsc

---
+++ DummyProjectGit/src/test/java/Sample/Logout.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class Logout {
+
+	public static void main(String[] args) 
+	{
+		
+      System.out.println("logout");
+	}
+
+}
---
+++ DummyProjectGit/src/test/java/Sample/Register.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class Register {
+
+	public static void main(String[] args)
+	{
+	  System.out.println("register");
+
+	}
+
+}
